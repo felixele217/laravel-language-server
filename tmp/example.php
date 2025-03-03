7b591b1c9d8bd65dfaf0e57acde892659c85c4db
felix-droese <felix.droese@clockin.de>
2025-03-03 20:48:14 +0100

    scaffold wip

--- tmp/example.php
+++ tmp/example.php
@@ -1,3 +1,19 @@
 <?php
 
-echo 'hallo';
+declare(strict_types=1);
+
+class Controller
+{
+    public function index()
+    {
+        return Inertia::render('Dashboard/DashboardPage.vue', [
+            'name' => 'John Doe',
+            'age' => 30,
+        ]);
+
+        return Inertia::render('timesheet/TimesheetPage.vue', [
+            'name' => 'John Doe',
+            'age' => 30,
+        ]);
+    }
+}
---
+++ tmp/php/Inertia.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+class Inertia
+{
+    public static function render(string $viewName, array $daya): void
+    {
+        echo $viewName;
+    }
+}
