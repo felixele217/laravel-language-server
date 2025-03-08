3c8870128bd200aed9fb1e530bd38056df3822ef
felix-droese <felix.droese@clockin.de>
2025-03-08 19:13:32 +0100

    wip

--- server/src/methods/textDocument/completion.ts
+++ server/src/methods/textDocument/completion.ts
@@ -8,6 +8,7 @@ import { TextDocumentPositionParams } from "./definition";
 import { getBladeCompletionItems } from "../../utils/completion/getBladeCompletionItems";
 import * as fs from "fs";
 import * as path from "path";
+import { getInertiaPageNames } from "../../utils/completion/getInertiaCompletionItems";
 
 export type CompletionItem = {
   label: string;
@@ -57,67 +58,4 @@ function getCompletionItems(currentWord: Word): CompletionItem[] {
     return getBladeCompletionItems(currentWord);
   }
   return [];
-}
-
-function getInertiaPageNames(currentWord: Word): CompletionItem[] {
-  const items: CompletionItem[] = [];
-  try {
-    const searchTerm = extractInertiaSearchTerm(currentWord.text);
-    const pagesDir = inertiaPagesDir;
-
-    if (!fs.existsSync(pagesDir)) return items;
-
-    const firstLevelItems = fs.readdirSync(pagesDir, { withFileTypes: true });
-    return [
-      ...getTopLevelPages(firstLevelItems, searchTerm),
-      ...getNestedPages(firstLevelItems, pagesDir, searchTerm),
-    ];
-  } catch (error) {
-    console.error("Failed to read Inertia pages directory:", error);
-    return items;
-  }
-}
-
-function extractInertiaSearchTerm(text: string): string {
-  return text.match(/Inertia::render\('([^']+)/)?.[1] || "";
-}
-
-function getTopLevelPages(
-  items: fs.Dirent[],
-  searchTerm: string,
-): CompletionItem[] {
-  return items
-    .filter((item) => item.isFile() && item.name.endsWith(".vue"))
-    .map((item) => {
-      const pagePath = item.name.replace(/\.vue$/, "");
-      if (pagePath.toLowerCase().includes(searchTerm.toLowerCase())) {
-        return { label: pagePath };
-      }
-      return null;
-    })
-    .filter((item): item is CompletionItem => item !== null);
-}
-
-function getNestedPages(
-  items: fs.Dirent[],
-  pagesDir: string,
-  searchTerm: string,
-): CompletionItem[] {
-  return items
-    .filter((item) => item.isDirectory())
-    .flatMap((dir) => {
-      const nestedPath = path.join(pagesDir, dir.name);
-      const nestedItems = fs.readdirSync(nestedPath, { withFileTypes: true });
-
-      return nestedItems
-        .filter((item) => item.isFile() && item.name.endsWith(".vue"))
-        .map((item) => {
-          const pagePath = `${dir.name}/${item.name.replace(/\.vue$/, "")}`;
-          if (pagePath.toLowerCase().includes(searchTerm.toLowerCase())) {
-            return { label: pagePath };
-          }
-          return null;
-        })
-        .filter((item): item is CompletionItem => item !== null);
-    });
 }
