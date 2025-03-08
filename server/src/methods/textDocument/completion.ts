c3956385583ec442dd7e350d85c196f638f670b5
felix-droese <felix.droese@clockin.de>
2025-03-08 19:12:38 +0100

    move blade completin items

--- server/src/methods/textDocument/completion.ts
+++ server/src/methods/textDocument/completion.ts
@@ -5,10 +5,11 @@ import { RequestMessage } from "../../server";
 import { Range } from "../../types";
 import { Word, wordUnderCursor } from "../../utils/Word";
 import { TextDocumentPositionParams } from "./definition";
+import { getBladeCompletionItems } from "../../utils/completion/getBladeCompletionItems";
 import * as fs from "fs";
 import * as path from "path";
 
-type CompletionItem = {
+export type CompletionItem = {
   label: string;
   textEdit?: TextEdit;
 };
@@ -53,7 +54,7 @@ function getCompletionItems(currentWord: Word): CompletionItem[] {
     return getInertiaPageNames(currentWord);
   }
   if (currentWord.type === "blade-view") {
-    return getBladeViewNames(currentWord);
+    return getBladeCompletionItems(currentWord);
   }
   return [];
 }
@@ -119,97 +120,4 @@ function getNestedPages(
         })
         .filter((item): item is CompletionItem => item !== null);
     });
-}
-
-function getBladeViewNames(currentWord: Word): CompletionItem[] {
-  const items: CompletionItem[] = [];
-  try {
-    const modulesDir = path.resolve(process.cwd(), "modules");
-    // Check if we're looking for module views
-    if (currentWord.text.includes("::")) {
-      if (fs.existsSync(modulesDir)) {
-        return getAllModuleViews(modulesDir);
-      }
-      return items;
-    }
-
-    // Handle default views from resources/views
-    const defaultViewsDir = path.resolve(process.cwd(), "resources", "views");
-    if (!fs.existsSync(defaultViewsDir)) return items;
-
-    items.push(...getAllModuleViews(modulesDir));
-    items.push(...getDefaultViews(defaultViewsDir));
-
-    return items;
-  } catch (error) {
-    console.error("Failed to read blade views:", error);
-    return items;
-  }
-}
-
-function getDefaultViews(viewsDir: string): CompletionItem[] {
-  const bladeFiles = getAllBladeFiles(viewsDir);
-
-  return bladeFiles.map((file) => {
-    const normalizedPath = normalizeViewPath(file, viewsDir);
-
-    return { label: normalizedPath };
-  });
-}
-
-function getAllModuleViews(modulesDir: string): CompletionItem[] {
-  const items: CompletionItem[] = [];
-  const modules = fs
-    .readdirSync(modulesDir, { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory());
-
-  for (const moduleDir of modules) {
-    const viewsPath = path.join(
-      modulesDir,
-      moduleDir.name,
-      "Resources",
-      "views",
-    );
-    if (!fs.existsSync(viewsPath)) continue;
-
-    const bladeFiles = getAllBladeFiles(viewsPath);
-    items.push(...createCompletionItems(bladeFiles, viewsPath, moduleDir.name));
-  }
-
-  return items;
-}
-
-function createCompletionItems(
-  files: string[],
-  viewsPath: string,
-  moduleName: string,
-): CompletionItem[] {
-  return files.map((file) => {
-    const normalizedPath = normalizeViewPath(file, viewsPath);
-    const fullViewPath = `${moduleName.toLowerCase()}::${normalizedPath}`;
-
-    return { label: fullViewPath };
-  });
-}
-
-function normalizeViewPath(file: string, viewsPath: string): string {
-  return path
-    .relative(viewsPath, file)
-    .replace(/\.blade\.php$/, "")
-    .split(path.sep)
-    .join(".");
-}
-
-function getAllBladeFiles(dir: string): string[] {
-  const items = fs.readdirSync(dir, { withFileTypes: true });
-  return items.reduce<string[]>((results, item) => {
-    const fullPath = path.join(dir, item.name);
-    if (item.isDirectory()) {
-      return results.concat(getAllBladeFiles(fullPath));
-    }
-    if (item.isFile() && item.name.endsWith(".blade.php")) {
-      results.push(fullPath);
-    }
-    return results;
-  }, []);
 }
